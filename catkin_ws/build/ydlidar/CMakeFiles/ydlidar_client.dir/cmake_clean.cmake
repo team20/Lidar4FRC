bb7bd61fea298ea8738639aa4d46cf891961079f
23watejona <jonathan@waters.to>
2021-01-12 13:23:56 -0500

    initial commit

---
+++ catkin_ws/build/ydlidar/CMakeFiles/ydlidar_client.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/ros/catkin_ws/devel/lib/ydlidar/ydlidar_client"
+  "/home/ros/catkin_ws/devel/lib/ydlidar/ydlidar_client.pdb"
+  "CMakeFiles/ydlidar_client.dir/src/ydlidar_client.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ydlidar_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
