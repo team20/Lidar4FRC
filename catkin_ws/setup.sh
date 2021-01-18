df705743a31f31f40715239626830af547b89772
23watejona <jonathan@waters.to>
2021-01-18 18:02:30 -0500

    updating git repo

--- catkin_ws/setup.sh
+++ catkin_ws/setup.sh
@@ -2,5 +2,5 @@ echo "Setting up ros"
 source $ROS_HOME/setup.sh
 echo "Setting up catkin workspace"
 source $CATKIN_WS_HOME/devel/setup.bash
-#roslaunch start run.launch
-roslaunch ydlidar_ros gazebo.launch
+roslaunch start run.launch
+#roslaunch ydlidar_ros gazebo.launch
